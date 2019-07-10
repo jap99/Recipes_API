5ab9f312f605b30b34dd2e7b4894a1b60c26458f
javidpoornasir <javidp.sf@gmail.com>
2019-07-10 01:54:59 -0700

    validate user provided value in email field when creating user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
+
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@samscloud.com'
@@ -23,3 +24,10 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+
+
+    def test_new_user_invalid_email(self):
+        """Test creating user w/o email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
