3bc1bae58b0851a57db47b10cdb7f95fb4d1ea50
javidpoornasir <javidp.sf@gmail.com>
2019-07-17 05:39:06 -0700

    create tests - for manager user endpiont

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,19 +11,22 @@ from rest_framework import status # module that has status codes // makes tests 
 
 CREATE_USER_URL = reverse('user:create') # based on the app & URL in urls.py // app = user // will look in the 'user' app for a url called 'create' 
 TOKEN_URL = reverse('user:token')
-
+ME_URL = reverse('user:me')
 
 def create_user(**params):  # can pass in many params // dynamic list of args.  # this method is used to easily create a user when the test requires that a user already exists (for example: test_user_exists()).
     return get_user_model().objects.create_user(**params)
    
 
 
-"""TEST CLASS"""         
+"""PUBLIC TEST CLASS"""         
 
 class PublicUserAPITests(TestCase):      
 
     def setUp(self):     
         self.client = APIClient()
+
+
+# CREATE USER (CREATE_USER_URL)
 
     def test_user_is_created_successfully(self):  
         payload = {
@@ -66,7 +69,7 @@ class PublicUserAPITests(TestCase):
         self.assertFalse(user_exists)                                       
 
 
-# TOKENS
+# TOKENS (TOKEN_URL)
 
     def test_token_was_created_for_user(self):
         payload = {'email': 'test@cleandev.com', 'password': 'testpass'}
@@ -97,5 +100,68 @@ class PublicUserAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
+# MANAGE USER (ME_URL)
 
-"""TEST CLASS"""
+    def test_that_authentication_is_required_for_the_manage_user_endpoint(self):
+        """Test that authentication is required for users"""
+        response = self.client.get(ME_URL)
+        # make sure we get the correct status code for unauthorized request to the endpoint
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
+"""PRIVATE TEST CLASS"""
+
+
+class PrivateUserAPITests(TestCase):
+    
+    # only doing authentication in the setup & not each time for each test
+    def setUp(self):
+        self.user = create_user(
+            email='test@cleandev.com',
+            password='testpass',
+            name='sample name'
+        )
+        self.client = APIClient()
+        # use the force authenticate helper method to auth. any request made with our sample user
+        self.client.force_authenticate(user=self.user)
+
+        
+
+    
+        # MANAGE USER (ME_URL)
+
+        def test_profile_is_retrieved_successfully(self):
+            """Test that profile is retrieved for authenticated user"""
+            response = self.client.get(ME_URL)
+            # assert status code is 200
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            # test we got back the correct user
+            self.assertEqual(response.data, {
+                'name': self.user.name,
+                'email': self.user.email
+            })
+
+
+        def test_user_profile_is_modified_successfully(self):
+            """Test that authenticated user can successfully make changes to their profile"""
+            payload = {
+                'name': 'new name sample',
+                'password': 'newpass1234'
+            }
+            res = self.client.patch(ME_URL, payload)
+            # update the user with the latest values on the db // refresh_from_db() is django method
+            self.user.refresh_from_db()
+            # assert all the values we provided were updated correctly
+            self.assertEqual(self.user.name, payload['name'])
+            self.assertTrue(self.user.check_password(payload['password']))
+            # assert correct status code
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+        def test_POST_request_on_me_url_not_allowed(self):
+            res = self.client.post(ME_URL, {})
+            # assert for 405 status code
+            self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
