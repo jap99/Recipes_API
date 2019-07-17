5f4a69d69a6269a978065ca593e2c9d90ae9601a
javidpoornasir <javidp.sf@gmail.com>
2019-07-17 08:57:52 -0700

    test_models.py - test tag's created & converts to correct string representation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+# create users in our tests
+def create_sample_user(email='test@cleandev.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
+
 
 
 class ModelTests(TestCase):
-
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
@@ -13,26 +18,19 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password)) # check_password comes with django
-
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized - all lower string"""
         email = 'test@Javid.com'
         user = get_user_model().objects.create_user(email, 'password123')
-
         self.assertEqual(user.email, email.lower())
-
-
 
     def test_new_user_invalid_email(self):
         """Test creating user w/o email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'password123')
-
-
 
     def test_create_new_superuser(self):
         """Test creating a new super user"""
@@ -40,6 +38,14 @@ class ModelTests(TestCase):
             'test@javid.com',
             'password123'
         )
-
         self.assertTrue(user.is_superuser) # included as part of PermissionsMixin
         self.assertTrue(user.is_staff)
+
+    def test_tag_is_created_and_converts_to_correct_string_representation(self):
+        # create our tag
+            # give it a name we may use in our system
+        tag = models.Tag.objects.create(
+            user=create_sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
