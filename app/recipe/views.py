0b7a8093cfe7cb6462fc96b0de2ebd400ffee66b
javidpoornasir <javidp.sf@gmail.com>
2019-07-17 10:08:28 -0700

    Add serializer.py & views.py - for Tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# the views we'll create is from a combination of generic view set & model mixin
+    # we're going to base it off the generic view set
+        # specifically, we'll use the list model mixin
+        # we're able to pull in different parts of a view for our app
+            # we only want the LIST function (not create, update, or delete functions)
+from rest_framework import viewsets, mixins 
 
-# Create your views here.
+# to auth. the requests
+from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.permissions import IsAuthenticated
+
+# import the tag and the serializer
+from core.models import Tag 
+from recipe import serializers
+
+
+# create our viewset
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the database"""
+    # add the authentication & permission classes 
+        # requires that token auth. is used
+        authentication_classes = (TokenAuthentication,)
+        # requires that the user's auth. to use the api
+        permission_classes = (IsAuthenticated,)
+        # add the query set so when we're defining a list model mixin in the generic view set we
+            # need to provide the query set we want to return 
+        queryset = Tag.objects.all()
+        serializer_class = serializers.TagSerializer
