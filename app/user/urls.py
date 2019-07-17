532f5e1ba009044c299a174e6fcedebaa4010027
javidpoornasir <javidp.sf@gmail.com>
2019-07-17 01:03:05 -0700

    urls.py - add /user/create endpoint - links to CreateUserView, gave it the name 'create'

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,3 +4,12 @@ from django.urls import path
 # import our view
 from user import views 
 
+# define our app name - lets us identify which app we're creating the URL from when using reverse()
+app_name = 'user'
+
+urlpatterns = [
+    # first: our api will be user/create, 
+    # second: the view we wire the URL to, 
+    # third: give it a name we can use when using django's reverse() lookup function
+    path('create/', views.CreateUserView.as_view(), name='create'),    
+]
