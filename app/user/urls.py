4a9f86959d8eaf3b3fbcde4c9c007b320b4f86b5
javidpoornasir <javidp.sf@gmail.com>
2019-07-17 01:10:42 -0700

    urls.py - tell the app directory to pass requests with the /api/user/ path to the user directory's urls.py module/file

--- app/user/urls.py
+++ app/user/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     # first: our api will be user/create, 
     # second: the view we wire the URL to, 
     # third: give it a name we can use when using django's reverse() lookup function
-    path('create/', views.CreateUserView.as_view(), name='create'),    
+    path('create/', views.CreateUserView.as_view(), name='create'),    # if path matches create/, we go to that view which will handle/render our API request
+    
 ]
+
